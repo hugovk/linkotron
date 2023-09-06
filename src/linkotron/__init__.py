778b4e0349283faf2e0f9eb13a2a66b5543444b9
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-09-06 18:27:40 +0300

    Fix bug with line prefixes and suffixes when formatting as RST

--- src/linkotron/__init__.py
+++ src/linkotron/__init__.py
@@ -65,5 +65,5 @@ def shorten(line: str, *, formatter: str | None = None) -> str:
     if formatter in ("md", "markdown"):
         return f"{prefix}[{short}]({long}){suffix}"
     elif formatter in ("rst", "restructuredtext"):
-        return f"`{prefix}{short} <{long}>`__{suffix}"
+        return f"{prefix}`{short} <{long}>`__{suffix}"
     return short
